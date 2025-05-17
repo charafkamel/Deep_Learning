55c16dab2f34fc78574f80938728048a066004b7
Charaf Kamel <charaf@haas001.ds-a4-r02.cct.rcp.epfl.ch>
2025-05-17 15:32:29 +0200

    gitignore test

--- basic_running_scripts.sh
+++
@@ -1,5 +0,0 @@
-runai submit --image registry.rcp.epfl.ch/ee-559-charaf/my-toolbox:v0.1 --gpu 1 --pvc course-ee-559-scratch:/scratch --pvc home:/pvc/home test-job --command -- echo "Hello"
-
-runai submit --image registry.rcp.epfl.ch/ee-559-charaf/my-toolbox:v0.2 --gpu 1 --pvc course-ee-559-scratch:/scratch --pvc home:/pvc/home test-job --command -- sh -c "cd ../../pvc/home/DeepLearning/Project/Deep_Learning/ && sleep 20000"
-
-runai submit --image registry.rcp.epfl.ch/ee-559-charaf/my-toolbox:v0.2 --gpu 1 --pvc course-ee-559-scratch:/scratch --pvc home:/pvc/home test-job --command -- sh -c "cd ../../pvc/home/DeepLearning/Project/Deep_Learning/ && pip install flash-attn==2.7.3 && python sft_training.py"
